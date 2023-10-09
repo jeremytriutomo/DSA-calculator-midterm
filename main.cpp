1a77cd863a1741b84dce2bcf21798a9fa561de94
jeremytriutomo <jeremytriutomo720@gmail.com>
2023-10-09 13:54:12 +0700

    by Jeremy and Nicole: create history feature

--- main.cpp
+++ main.cpp
@@ -27,51 +27,81 @@ int main()
     cout << "Square Root: R" << endl;
     cout << "Logarithm: L" << endl;
     cout << "Natural Logarithm: N" << endl;
-    cout << "Press H and Enter if you want to display history" << endl;
-    cout << "Enter \"X0\" and Enter if you want to EXIT" << endl;
     cout << "================================" << endl;
 
-    LinkedList history;
+    int outer_loop = 1;
 
-    cout << "\nEnter your calculation:";
-    cin >> user_operand1 >> user_operator >> user_operand2;
-
-    Terms term1;
-    term1.set_value(user_operand1, user_operator, user_operand2);
-    term1.set_result();
-    term1.get_result();
-
-    history.insert(term1.result);
-    cout << "\n Printing the list: " << endl;
-    history.print();
-
-    int counter = 1;
-
-    while (counter = 1)
+    while (outer_loop = 1)
     {
-        user_operand1 = history.return_result();
-        cout << "\nEnter your calculation: " << user_operand1 << " ";
-        cin >> user_operator >> user_operand2;
 
-        // cout << "\nTesting parsing: " << endl;
-        // cout << user_operand1 << endl;
-        // cout << user_operator << endl;
-        // cout << user_operand2 << endl;
-        if (user_operator == 'X')
+        LinkedList history;
+        cout << "Type \"0X0\" and Enter to EXIT" << endl;
+        cout << "\nEnter your calculation:";
+        cin >> user_operand1 >> user_operator >> user_operand2;
+
+        if (user_operand1 == 0 && (user_operator == 'X' || user_operator == 'x') && user_operand2 == 0)
         {
-            counter = 0;
             break;
         }
         else
         {
-            Terms term2;
-            term2.set_value(user_operand1, user_operator, user_operand2);
-            term2.set_result();
-            term2.get_result();
 
-            history.insert(term2.result);
-            cout << "\n Printing the list: " << endl;
-            history.print();
+            Terms term1;
+            term1.set_value(user_operand1, user_operator, user_operand2);
+            term1.set_result();
+            term1.get_result();
+
+            history.insert(term1.result);
+            // cout << "\n History: " << endl;
+            // history.print();
+
+            int counter = 1;
+
+            while (counter = 1)
+            {
+                user_operand1 = history.return_result();
+
+                cout << "\n\nType \"H0\" and Enter to display HISTORY" << endl;
+                cout << "Type \"X0\" and Enter to EXIT" << endl;
+                cout << "\nEnter your calculation: " << user_operand1 << " ";
+                cin >> user_operator >> user_operand2;
+
+                // cout << "\nTesting parsing: " << endl;
+                // cout << user_operand1 << endl;
+                // cout << user_operator << endl;
+                // cout << user_operand2 << endl;
+                if (user_operator == 'X' || user_operator == 'x')
+                {
+                    counter = 0;
+                    break;
+                }
+                else if (user_operator == 'H' || user_operator == 'h')
+                {
+                    cout << "\n History: " << endl;
+                    history.print();
+                    cout << "\nPress D to Delete Press C to Continue: ";
+                    char user_history_input;
+                    cin >> user_history_input;
+                    if (user_history_input == 'D' || user_history_input == 'd')
+                    {
+                        history.clear();
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    Terms term2;
+                    term2.set_value(user_operand1, user_operator, user_operand2);
+                    term2.set_result();
+                    term2.get_result();
+
+                    history.insert(term2.result);
+                }
+            }
         }
     }
 }
